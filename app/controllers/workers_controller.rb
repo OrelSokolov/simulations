3fbd251f719b7ea81132153c280f203eae78877e
Oleg Orlov <orelcokolov@gmail.com>
2019-11-06 13:11:08 +0300

    Order workers by updated_at field

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -4,7 +4,7 @@ class WorkersController < ApplicationController
   # GET /workers
   # GET /workers.json
   def index
-    @workers = Worker.all
+    @workers = Worker.all.order(updated_at: :desc)
   end
 
   # GET /workers/1
