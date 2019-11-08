eb7a941f948a0cee2da67f75332ad512e31d04a1
Oleg Orlov <orelcokolov@gmail.com>
2019-11-08 12:55:50 +0300

    Save engine version for worker in DB

---
+++ db/migrate/20191108095510_add_engine_version_to_worker.rb
@@ -0,0 +1,5 @@
+class AddEngineVersionToWorker < ActiveRecord::Migration[6.0]
+  def change
+    add_column :workers, :engine_string, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_11_102507) do
+ActiveRecord::Schema.define(version: 2019_11_08_095510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_10_11_102507) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "blocked", default: false
     t.string "worker_version"
+    t.string "engine_string"
   end
 
 end
