0d9a5f619e753c0ac813c04386b821847b4b5389
Oleg Orlov <orelcokolov@gmail.com>
2019-12-09 18:28:57 +0300

    Refactor form

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,12 @@
 module TasksHelper
+
+  def task_checkbox(task, field)
+    content_tag :div, class: "custom-control custom-switch" do
+      concat(
+          check_box("task[#{field}]", field, checked: task.send(field), class: 'custom-control-input', id: field.to_s) +
+          label_tag(field, nil, class: 'custom-control-label', for: field.to_s)
+      )
+    end
+  end
+
 end
